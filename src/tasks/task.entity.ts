7b82825972c19ad768b8cf5b6228387cc248078d
TestSubjectUser <wami50731@gmail.com>
2025-02-24 18:19:27 +0530

    setup db conn, create task entity

---
+++ src/tasks/task.entity.ts
@@ -0,0 +1,17 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { taskStatus } from './tasks.model';
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: taskStatus;
+}
